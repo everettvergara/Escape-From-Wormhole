33dc04601ee7365ef5c6951faf22c97eeeab408a
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-01 07:49:57 +0800

    added next() in QuadBezierAnim

--- include/Bezier.hpp
+++ include/Bezier.hpp
@@ -27,13 +27,31 @@ namespace g80 {
     };
     using Color = Uint32;
 
-    // class Bezier {
-    // public:
-        
+    class QuadBezier {
+    public:
+        QuadBezier(const Point &p1, const Point &p2, const Point &p3, const Dim smax) : 
+        p1_(p1), p2_(p2), p3_(p3), smax_(smax), d1_(p2_ - p1_), d2_(p3_ - p2_) {}
 
-    // private:
-    //     Point p1, p2, p3;
-    //     Dim s, smax;
-    // }
+        auto next() -> Point & {
+            Point c1, c2;
+            c1.x = p1.x + d1.x * s / smax;
+            c1.y = p1.y + d1.y * s / smax;
+            c2.x = p2.x + d2.x * s / smax;
+            c2.y = p2.y + d2.y * s / smax;
+            
+            Point dc = c2 - c1;
+            Point bz;
+            bz.x = c1.x + dc.x * s / smax;
+            bz.y = c1.x + dc.y * s / smax;
+            ++s;
+            return bz;
+        }
+
+    private:
+        Point p1_, p2_, p3_;
+        Dim s_{0}, smax_;
+        Point d1_, d2_;
+        
+    }
 }
 #endif 
--- wormhole.cpp
+++ wormhole.cpp
@@ -3,11 +3,11 @@
 
 using namespace g80;
 auto main(int argc, const char *argv[]) -> int {
-    Point p1{10, 10};
-    Point p2{10, 10};
+    // Point p1{10, 10};
+    // Point p2{10, 10};
 
-    p1 = p1 + p2;
+    // p1 = p1 + p2;
 
-    std::cout << p1.x << ", " << p1.y << "\n";
+    // std::cout << p1.x << ", " << p1.y << "\n";
 
 }
