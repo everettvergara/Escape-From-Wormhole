3c511aabe2f70c245f8e5cd559b6e377b0ce3e7d
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-15 22:26:36 +0800

    misc bug fixes

--- include/PointMotion.hpp
+++ include/PointMotion.hpp
@@ -1,19 +1,23 @@
 #ifndef _POINTMOTION_HPP_
 #define _POINTMOTION_HPP_
 
-
+#include <type_traits>
 #include "Point.hpp"
 
 namespace g80 {
 
     template<typename T>
     class PointMotion {
+    static_assert(
+        std::is_same<T, float>::value ||
+        std::is_same<T, double>::value ||
+        std::is_same<T, long double>::value, "Type must be float, double or long double only.");
 
     public:
         PointMotion(const Point<T> &start_point, const Sint32 step_size, const Sint32 trail_size) : 
             head_(start_point),
             tail_(start_point),
-            step_size_(step_size), trail_size_(trail_size), tail_step_(-trail_size_) {}
+            step_size_(step_size), tail_step_(-trail_size) {}
 
         virtual auto reset() -> void {};
         virtual auto next() -> bool {return true;};
@@ -24,35 +28,37 @@ namespace g80 {
     protected:
         Point<T> head_;
         Point<T> tail_;
-        Sint32 step_size_, trail_size_, current_step_{0}, tail_step_{0};
+        Sint32 step_size_, current_step_{0}, tail_step_{0};
     };
 
     template<typename T>
     class LineMotion : public PointMotion<T> {
     public:
         LineMotion(const Point<T> &start_point, const Point<T> &end_point, Sint32 step_size, Sint32 trail_size) : 
-            PointMotion<T>(start_point, step_size, trail_size),
-            delta_(end_point - start_point) {
-            x_inc_ = delta_.x / this->step_size_;
-            y_inc_ = delta_.y / this->step_size_;
+            PointMotion<T>(start_point, step_size, trail_size) {
+            
+            Point<T> delta {end_point - start_point};
+            x_inc_ = delta.x / this->step_size_;
+            y_inc_ = delta.y / this->step_size_;
         }
 
         auto next() -> bool {
-            if (tail_step_ == this->step_size_) return false;
+            if (this->tail_step_ == this->step_size_) return false;
             
             this->head_.x += x_inc_;
             this->head_.y += y_inc_;
-
-            if (tail_step_++ >= 0) {
+        
+            if (this->tail_step_++ >= 0) {
                 this->tail_.x += x_inc_;
                 this->tail_.y += y_inc_;                
             }
+
+            ++this->current_step_;
 
             return true;
         }
 
     private:
-        Point<T> delta_;
         T x_inc_, y_inc_;
     };
 }
