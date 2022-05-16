82a054cab3a1e0da07a7df1cde6ed7706a77b23d
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-16 17:23:12 +0800

    LineMotion with Accel

--- include/LineMotion.hpp
+++ include/LineMotion.hpp
@@ -18,48 +18,33 @@ namespace g80 {
             const Sint32 sz_trail) {
             
             this->set(start_point, sz_steps, sz_trail);
-            start_ = start_point;
+
+            start_point_ = start_point;
+
+            // Accel x,y = k * s^2
             Point<T> delta {end_point - start_point};
-            // head_inc_.x = delta.x / this->sz_steps_;
-            // head_inc_.y = delta.y / this->sz_steps_;
-            // tail_inc_ = head_inc_;
-
-            // Accelaration 
-            // e = 2
-            // Accel in x,y = k * s^e;
-            // 
-            // Compute for k:
-            
-            float step_sq =  this->sz_steps_ * this->sz_steps_;
-            k_x_ = delta.x / step_sq;
-            k_y_ = delta.y / step_sq;
-
-            // k = xy / (s^e);
-            
+            k_ = delta / (this->sz_steps_ * this->sz_steps_);
         }
 
         auto next() -> bool {
             if (this->tail_step_ == this->sz_steps_) return false;
 
-            
-            float step_sq = this->head_step_ * this->head_step_; 
-            this->head_.x = start_.x + k_x_ * step_sq;
-            this->head_.y = start_.y + k_y_ * step_sq;
-            ++this->head_step_;
+            if (this->head_step_ < this->sz_steps_) {
+                float step_sq = this->head_step_ * this->head_step_; 
+                this->head_ = start_point_ + k_ * step_sq;
+                ++this->head_step_;
+            }
 
             if (this->tail_step_++ >= 0) {
                 float tail_step_sq = this->tail_step_ * this->tail_step_; 
-                this->tail_.x = start_.x + k_x_ * tail_step_sq;
-                this->tail_.y = start_.y + k_y_ * tail_step_sq;                
+                this->tail_ = start_point_ + k_ * tail_step_sq;
             }
 
-            SDL_Log("%.2f, %.2f", this->head_.x, this->head_.y);
             return true;
         }
 
     private:
-        Point<T> start_;
-        float k_x_, k_y_;
+        Point<T> start_point_, k_;
     };
 }
 
