84f57d6b3c455edfb5b122a2f90c6e4107e4be55
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-20 07:07:24 +0800

    better circlemotiondemo

--- include/Demo/CircleMotionDemo.hpp
+++ include/Demo/CircleMotionDemo.hpp
@@ -16,10 +16,11 @@ namespace g80 {
         auto update_states() -> bool; 
 
     private:
-        const Sint32 TN_{3600}, N_{1000};
+        const Sint32 TN_{3600}, N_{10000};
         SinCache<float> sine_{TN_};
         CosCache<float> cosine_{TN_};      
         std::vector<CircleMotion<float>> whirls_;
+        Palette pal_;
     };
 
     CircleMotionDemo::CircleMotionDemo() : Video() {
@@ -36,33 +37,24 @@ namespace g80 {
                 10 + rand() % 100, rand() % 1000, 10, rand() % 3600, rand() % 3600, cosine_, sine_);
         } 
 
+        pal_.add_gradients(surface_->format, 
+            {
+                {0, SDL_MapRGBA(surface_->format, 0, 255, 255, 255)},
+                {60, SDL_MapRGBA(surface_->format, 0, 100, 255, 255)},
+                {120, SDL_MapRGBA(surface_->format, 100, 100, 255, 255)},
+                {180, SDL_MapRGBA(surface_->format, 255, 0, 255, 255)},
+                {300, SDL_MapRGBA(surface_->format, 255, 255, 255, 255)},});
         return true;
     }
 
     auto CircleMotionDemo::update_states() -> bool {
-
-        // Palette pal, pal2;
-        // pal.add_gradients(surface_->format, 
-        //     {
-        //         {0, SDL_MapRGBA(surface_->format, 0, 255, 255, 255)},
-        //         {60, SDL_MapRGBA(surface_->format, 0, 100, 255, 255)},
-        //         {120, SDL_MapRGBA(surface_->format, 100, 100, 255, 255)},
-        //         {180, SDL_MapRGBA(surface_->format, 255, 0, 255, 255)},
-        //         {300, SDL_MapRGBA(surface_->format, 255, 255, 255, 255)},});
-                
-        // pal2.add_gradients(surface_->format,
-        //     {
-        //         {0, SDL_MapRGBA(surface_->format, 255, 255, 255, 255)},
-        //         {100, SDL_MapRGBA(surface_->format, 150, 0, 255, 255)},});
-                
-
         // Erase
         SDL_FillRect(surface_, NULL, 0);
         
         // Draw
-        RGBAColor c = SDL_MapRGBA(surface_->format, 255, 255, 255, 255);
         for (auto &w : whirls_) {
-            line(w.get_head(), w.get_tail(), c);  
+            Sint32 size_of_trail = w.get_head_step() - w.get_tail_step();
+            line(w.get_head(), w.get_tail(), pal_[static_cast<int>(300.0f * (w.get_tail_step() + size_of_trail) / (w.get_size_of_step() + size_of_trail))]);  
         }
 
         for (auto &w : whirls_) {
@@ -72,52 +64,6 @@ namespace g80 {
                     10 + rand() % 100, rand() % 1000, 10, rand() % 3600, rand() % 3600, cosine_, sine_);
             }  
         }
-
-        // pset(line_motion_.get_tail(), SDL_MapRGBA(surface_->format, 255, 0, 0, 255));
-        // pset(line_motion_.get_head(), SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-
-        // line(line_motion_.get_head(), line_motion_.get_tail(), SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-
-        // if (!line_motion_.next())
-        //     exit(0);
-
-
-        // Palette pal;
-        // pal.add_gradients(surface_->format, 40, 
-        //     {
-        //         {0, SDL_MapRGBA(surface_->format, 255, 255, 255, 255)},
-        //         {10, SDL_MapRGBA(surface_->format, 255, 255, 0, 255)},
-        //         {20, SDL_MapRGBA(surface_->format, 255, 100, 0, 255)},
-        //         {30, SDL_MapRGBA(surface_->format, 255, 0, 0, 255)},
-        //         {40, SDL_MapRGBA(surface_->format, 255, 0, 255, 255)},});
-        
-        // // for (int i = 0; i < surface_->w - 1; ++i) {
-        // //     int h =  100 + rand() % 500;
-        // //     line_lite({i, surface_->h - 1}, {i, surface_->h - h}, pal, 0, 39);
-
-        // // }
-        
-        // line({50, 700}, {300, 50}, SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-        // line({300, 50}, {900, 300}, SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-        // line({900, 300}, {700, 50}, SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-        // line({700, 50}, {1000, 30}, SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-        // line({1000, 30}, {1279, 700}, SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-
-        // catmullrom_spline({{50, 700}, {300, 50}, {900, 300}, {700, 50}, {1000, 30}, {1279, 700}}, 100, pal, 0, 39);
-        // catmullrom_spline_lite({{50, 700}, {300, 50}, {900, 300}, {700, 50}, {1000, 30}, {1279, 700}}, 50, SDL_MapRGBA(surface_->format, 255, 0, 0, 255));
-        // exit(0);
-        // quad_bezier_lite({50, 50}, {150,600}, {900,300}, 20, SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-        // quad_bezier_lite({900, 300}, {1280, 700}, {50, 50}, 20, SDL_MapRGBA(surface_->format, 255, 255, 255, 255));
-
-        // cubic_bezier_lite({50, 700}, {300, 0}, {900, 300}, {700, 50},  30, pal, 0, 39);
-        // cubic_bezier_lite({700, 50}, {900, 100}, {700, 300}, {50, 700},  30, SDL_MapRGBA(surface_->format, 0, 255, 0, 255));
-        // bezier_lite({{0, 100}, {200, 700}, {300, 0}, {400, 500}, {500,600}, {600, 50}, {1000,70}, {0, 100}},  100, pal, 0, 39);
-
-        // line({-1000, surface_->h / 2}, {+3000, surface_->h / 2}, pal, 0, 39);
-        // line({3000, surface_->h / 3}, {-1000, surface_->h / 3}, pal, 0, 39);
-        //line({-1000, surface_->h / 2}, {+1000, surface_->h / 2}, SDL_MapRGBA(surface_->format, 255, 0, 0, 255));
-
-        // quad_bezier_lite({50, 50}, {150,600}, {900,300}, 20, pal, 0, 39);
 
         return true;
     }
