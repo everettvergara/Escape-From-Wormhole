a6852d1debfde678728bb6810992838c9763a593
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-23 19:29:34 +0800

    fixed circlewithaccelmotion

--- include/Motion/CircleWithAccelMotion.hpp
+++ include/Motion/CircleWithAccelMotion.hpp
@@ -32,10 +32,7 @@ namespace g80 {
             sz_steps_ = {sz_steps};
             tail_step_ = {-sz_trail};
             head_step_ = {0};
-
-            head_angle_ = start_ix;
-            tail_angle_ = start_ix;
-            // inc_ = 1.0f * (end_ix - start_ix) / sz_steps;
+            
             k_ = 1.0f * (end_ix - start_ix) / (sz_steps * sz_steps);
         }
 
@@ -45,25 +42,26 @@ namespace g80 {
             float size = cosine_cache.get_size();
 
             if (this->head_step_ < this->sz_steps_) {
-                head_angle_ += inc_;
+                
+                float angle = k_ * (head_step_ * head_step_);
 
-                if (head_angle_ >= size) head_angle_ = head_angle_ - size;
-                else if (head_angle_ < 0.0f) head_angle_ = size + head_angle_;
+                if (angle >= size) angle = angle - size;
+                else if (angle < 0.0f) angle = size + angle;
 
-                this->head_.x = center_.x + radius_ * cosine_cache[static_cast<Sint32>(head_angle_)];
-                this->head_.y = center_.y + radius_ * sine_cache[static_cast<Sint32>(head_angle_)];
+                this->head_.x = center_.x + radius_ * cosine_cache[static_cast<Sint32>(angle)];
+                this->head_.y = center_.y + radius_ * sine_cache[static_cast<Sint32>(angle)];
 
                 ++this->head_step_;
             }
 
             if (this->tail_step_++ >= 0) {
-                tail_angle_ += inc_;
+                float angle = k_ * (tail_step_ * tail_step_);
 
-                if (tail_angle_ >= size) tail_angle_ = tail_angle_ - size;
-                else if (tail_angle_ < 0.0f) tail_angle_ = size + tail_angle_;
+                if (angle >= size) angle = angle - size;
+                else if (angle < 0.0f) angle = size + angle;
 
-                this->tail_.x = center_.x + radius_ * cosine_cache[static_cast<Sint32>(tail_angle_)];
-                this->tail_.y = center_.y + radius_ * sine_cache[static_cast<Sint32>(tail_angle_)];
+                this->tail_.x = center_.x + radius_ * cosine_cache[static_cast<Sint32>(angle)];
+                this->tail_.y = center_.y + radius_ * sine_cache[static_cast<Sint32>(angle)];
             }
 
             return true;
@@ -81,7 +79,7 @@ namespace g80 {
         Sint32 sz_steps_, tail_step_, head_step_;
         Point<T> center_;
         Sint32 radius_;
-        T head_angle_, tail_angle_, inc_, k_;
+        T k_;
     };
 }
 
