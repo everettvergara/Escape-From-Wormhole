b0e964ad40f1f1988c1229af8de9cbd5798069af
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-25 20:18:52 +0800

    fixed bug

--- include/Demo/LissajousMotionDemo.hpp
+++ include/Demo/LissajousMotionDemo.hpp
@@ -19,7 +19,7 @@ namespace g80 {
         const Sint32 TN_{3600}, N_{1};
         SinCache<float> sine_{TN_};
         CosCache<float> cosine_{TN_};      
-        std::vector<Lissajous<float>> whirls_;
+        std::vector<LissajousMotion<float>> whirls_;
         Palette pal_;
     };
 
@@ -32,14 +32,13 @@ namespace g80 {
         whirls_.reserve(N_);
         for (int i = 0; i < N_; ++i) {
             whirls_.emplace_back();
-            // whirls_[i].lissajous_motion_set(
-            //     Lissajous<float> {
-            //         Point<float>(1280 / 2, 720 / 2),
-            //         300.0f, 150.0f,
-            //         1000.0f, 500.0f,
-            //         3000.0f, 2500.0f},
-                
-            //     1000, 10, cosine_, sine_);
+            whirls_[i].lissajous_motion_set(
+                Lissajous<float> {
+                    Point<float>(1280 / 2, 720 / 2),
+                    300.0f, 150.0f,
+                    1000.0f, 500.0f,
+                    3000.0f, 2500.0f},
+                1000, 10, cosine_, sine_);
         } 
 
         pal_.add_gradients(surface_->format, 
@@ -57,26 +56,23 @@ namespace g80 {
         // Erase
         SDL_FillRect(surface_, NULL, 0);
         
-        // // Draw
-        // for (auto &w : whirls_) {
-        //     Sint32 size_of_trail = w.get_head_step() - w.get_tail_step();
-        //     line(w.get_head(), w.get_tail(), pal_[static_cast<int>(300.0f * (w.get_tail_step() + size_of_trail) / (w.get_size_of_step() + size_of_trail))]);  
-        // }
+        // Draw
+        for (auto &w : whirls_) {
+            Sint32 size_of_trail = w.get_head_step() - w.get_tail_step();
+            line(w.get_head(), w.get_tail(), pal_[static_cast<int>(300.0f * (w.get_tail_step() + size_of_trail) / (w.get_size_of_step() + size_of_trail))]);  
+        }
 
-        // for (auto &w : whirls_) {
-        //     if (!w.next(cosine_, sine_)) {
-        //         w.lissajous_motion_set(
-        //             Lissajous<float> {
-        //                 Point<float> {1280 / 2, 720 /2};
-        //                 300.0f, 150.0f;
-        //                 1000.0f, 500.0f;
-        //                 3000.0f, 2500.0f},
-                
-        //             1000, 10, cosine_, sine_);
-        //         } 
-        //     }  
-        // }
-
+        for (auto &w : whirls_) {
+            if (!w.next(cosine_, sine_)) {
+                w.lissajous_motion_set(
+                    Lissajous<float> {
+                        Point<float> {1280 / 2, 720 /2},
+                        300.0f, 150.0f,
+                        1000.0f, 500.0f,
+                        3000.0f, 2500.0f},
+                    1000, 10, cosine_, sine_);
+                } 
+            }  
         return true;
     }
 }
