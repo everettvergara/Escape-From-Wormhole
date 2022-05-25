5c87325007e44fa84c2172e0f690d1b32c6433a6
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-25 20:34:21 +0800

    better demo

--- include/Demo/LissajousMotionDemo.hpp
+++ include/Demo/LissajousMotionDemo.hpp
@@ -6,6 +6,7 @@
 #include "Video.hpp"
 #include "Motion/LissajousMotion.hpp"
 #include "TrigCache.hpp"
+#include "LCMRND.hpp"
 
 namespace g80 {
     class LissajousMotionDemo : public Video {
@@ -16,7 +17,7 @@ namespace g80 {
         auto update_states() -> bool; 
 
     private:
-        const Sint32 TN_{3600}, N_{1};
+        const Sint32 TN_{3600}, N_{1000};
         SinCache<float> sine_{TN_};
         CosCache<float> cosine_{TN_};      
         std::vector<LissajousMotion<float>> whirls_;
@@ -32,13 +33,17 @@ namespace g80 {
         whirls_.reserve(N_);
         for (int i = 0; i < N_; ++i) {
             whirls_.emplace_back();
+            
+            float angle_x = 100.0f * (lcm_rnd() % 1000);
+            float angle_y = 100.0f * (lcm_rnd() % 1000);
+
             whirls_[i].lissajous_motion_set(
-                Lissajous<float> {
-                    Point<float>(1280 / 2, 720 / 2),
-                    300.0f, 150.0f,
-                    1000.0f, 500.0f,
-                    3000.0f, 2500.0f},
-                1000, 10, cosine_, sine_);
+                    Lissajous<float> {
+                        Point<float>{static_cast<float>(lcm_rnd() % 1280), static_cast<float>(lcm_rnd() % 720)},
+                        100.0f + (lcm_rnd() % 300), 100.0f + (lcm_rnd() % 300),
+                        angle_x, angle_y,
+                        angle_x + (lcm_rnd() % 3600), angle_y + (lcm_rnd() % 3600)},
+                    100, 10, cosine_, sine_);
         } 
 
         pal_.add_gradients(surface_->format, 
@@ -59,18 +64,23 @@ namespace g80 {
         // Draw
         for (auto &w : whirls_) {
             Sint32 size_of_trail = w.get_head_step() - w.get_tail_step();
+            //pset(w.get_head(), pal_[static_cast<int>(300.0f * (w.get_tail_step() + size_of_trail) / (w.get_size_of_step() + size_of_trail))]);  
             line(w.get_head(), w.get_tail(), pal_[static_cast<int>(300.0f * (w.get_tail_step() + size_of_trail) / (w.get_size_of_step() + size_of_trail))]);  
         }
 
         for (auto &w : whirls_) {
             if (!w.next(cosine_, sine_)) {
+
+                float angle_x = 100.0f * (lcm_rnd() % 1000);
+                float angle_y = 100.0f * (lcm_rnd() % 1000);
+
                 w.lissajous_motion_set(
                     Lissajous<float> {
-                        Point<float> {1280 / 2, 720 /2},
-                        300.0f, 150.0f,
-                        1000.0f, 500.0f,
-                        3000.0f, 2500.0f},
-                    1000, 10, cosine_, sine_);
+                        Point<float>{static_cast<float>(lcm_rnd() % 1280), static_cast<float>(lcm_rnd() % 720)},
+                        100.0f + (lcm_rnd() % 300), 100.0f + (lcm_rnd() % 300),
+                        angle_x, angle_y,
+                        angle_x + (lcm_rnd() % 3600), angle_y + (lcm_rnd() % 3600)},
+                    100, 10, cosine_, sine_);
                 } 
             }  
         return true;
