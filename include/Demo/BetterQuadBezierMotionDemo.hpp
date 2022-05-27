8af980561a0490696c952d3240b925f6e9ca7a18
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-27 23:54:14 +0800

    better demo

--- include/Demo/BetterQuadBezierMotionDemo.hpp
+++ include/Demo/BetterQuadBezierMotionDemo.hpp
@@ -19,7 +19,7 @@ namespace g80 {
         auto capture_events() -> bool;
 
     private:
-        const size_t TrigCacheN_{10000};
+        const size_t TrigCacheN_{20000};
         CosCache<float> cosine_{TrigCacheN_};
         SinCache<float> sine_{TrigCacheN_};
         Point<float> origin_;
