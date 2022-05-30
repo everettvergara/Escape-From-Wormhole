62ad67f050987ca1f2806f1dbf14d1f54c0466f1
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-05-30 22:26:18 +0800

    MISC EDITS

--- include/Motion/PropulsionMotion.hpp
+++ include/Motion/PropulsionMotion.hpp
@@ -28,10 +28,10 @@ namespace g80 {
     Inner outer distance decreases as the inner radius approaches origin.    
 
 */
-    template<typename T>
-    using Blasts = std::vector<LineWithAccelMotion<T>>;
+    template<typename F>
+    using Blasts = std::vector<LineWithAccelMotion<F>>;
 
-    template<typename T>
+    template<typename F>
     class PropulsionMotion {
     public:
         PropulsionMotion(
@@ -47,8 +47,8 @@ namespace g80 {
             const Sint32 inner_radius_dist_from_center,
             const Sint32 outer_radius_dist_from_center,
             const Sint32 blasts_n,
-            const CosCache<T> cosine,
-            const SinCache<T> sine,
+            const CosCache<F> cosine,
+            const SinCache<F> sine,
             const Sint32 aix,
             const Sint32 aox) -> void {
 
@@ -57,7 +57,7 @@ namespace g80 {
             outer_radius_ = {outer_radius};
             inner_radius_dist_from_center_ = {inner_radius_dist_from_center};
             outer_radius_dist_from_center_ = {outer_radius_dist_from_center};
-            f_ = {static_cast<T>(outer_radius_ / inner_radius_)};
+            outer_inner_ratio_ = {static_cast<F>(outer_radius_ / inner_radius_)};
 
             blasts_.reserve(blasts_n);
             for (Sint32 i = 0; i < blasts_n; ++i) {
@@ -67,7 +67,7 @@ namespace g80 {
                 Point<Sint32> p, t;
                 auto rnd_inner_angle_ix = lcm_rnd() % cosine.get_size();
                 auto rnd_inner_radius = 1 + lcm_rnd() % inner_radius_; 
-                auto rnd_inner_radiusf = rnd_inner_radius * f_;
+                auto rnd_inner_radiusf = rnd_inner_radius * outer_inner_ratio_;
                 p.x = get_inner_radius_center(cosine, sine, aix).x + rnd_inner_radius * cosine[rnd_inner_angle_ix]; 
                 p.y = get_inner_radius_center(cosine, sine, aix).y + rnd_inner_radius * sine[rnd_inner_angle_ix]; 
                 t.x = get_outer_radius_center(cosine, sine, aix).x + rnd_inner_radiusf * cosine[rnd_inner_angle_ix]; 
@@ -77,17 +77,17 @@ namespace g80 {
             }
         }
 
-        inline auto get_inner_radius_center(const CosCache<T> &cosine, const SinCache<T> &sine, const Sint32 aix) -> Point<Sint32> {
+        inline auto get_inner_radius_center(const CosCache<F> &cosine, const SinCache<F> &sine, const Sint32 aix) -> Point<Sint32> {
             return Point<Sint32>(center_.x + inner_radius_dist_from_center_ * cosine[aix], center_.y + inner_radius_dist_from_center_ * sine[aix] );
         }
 
-        inline auto get_outer_radius_center(const CosCache<T> &cosine, const SinCache<T> &sine, const Sint32 aix) -> Point<Sint32> {
+        inline auto get_outer_radius_center(const CosCache<F> &cosine, const SinCache<F> &sine, const Sint32 aix) -> Point<Sint32> {
             return Point<Sint32>(center_.x + outer_radius_dist_from_center_ * cosine[aix], center_.y + outer_radius_dist_from_center_ * sine[aix] );
         }
 
         auto next(
-            const CosCache<T> &cosine, 
-            const SinCache<T> &sine, 
+            const CosCache<F> &cosine, 
+            const SinCache<F> &sine, 
             const Sint32 inner_angle_ix, 
             const Sint32 origin_angle_ix) -> void {
 
@@ -101,10 +101,10 @@ namespace g80 {
             // T k = 1.0f ; // * trig_cache_half / (trig_cache_half * trig_cache_half);
             Sint32 outer_angle_ix = 1.025f * inner_angle_ix; // k * inner_angle_ix * inner_angle_ix;
             // SDL_Log("%.6f %d %d", k, outer_angle_ix, inner_angle_ix);
-            auto ref_outer_angle_ix = outer_angle_ix - origin_angle_ix;
-            if (ref_outer_angle_ix < 0) ref_outer_angle_ix = cosine.get_size() + ref_outer_angle_ix;
+            auto reouter_inner_ratio_outer_angle_ix = outer_angle_ix - origin_angle_ix;
+            if (reouter_inner_ratio_outer_angle_ix < 0) reouter_inner_ratio_outer_angle_ix = cosine.get_size() + reouter_inner_ratio_outer_angle_ix;
             
-            T lerp = 1.0f * (ref_outer_angle_ix >= trig_cache_half ? cosine.get_size() - ref_outer_angle_ix : ref_outer_angle_ix) / trig_cache_half;
+            F lerp = 1.0f * (reouter_inner_ratio_outer_angle_ix >= trig_cache_half ? cosine.get_size() - reouter_inner_ratio_outer_angle_ix : reouter_inner_ratio_outer_angle_ix) / trig_cache_half;
             auto couter_radius_dist_from_center = inner_radius_dist_from_center_ + lerp * (outer_radius_dist_from_center_ - inner_radius_dist_from_center_);
 
 
@@ -114,7 +114,7 @@ namespace g80 {
                     Point<Sint32> blast_from, blast_to;
                     auto rnd_inner_angle_ix = lcm_rnd() % cosine.get_size();
                     auto rnd_inner_radius = 1 + lcm_rnd() % inner_radius_; 
-                    auto rnd_outer_radius = rnd_inner_radius * f_;
+                    auto rnd_outer_radius = rnd_inner_radius * outer_inner_ratio_;
                     blast_from.x = (center_.x + inner_radius_dist_from_center_ * cosine[inner_angle_ix]) + rnd_inner_radius * cosine[rnd_inner_angle_ix];
                     blast_from.y = (center_.y + inner_radius_dist_from_center_ * sine[inner_angle_ix]) + rnd_inner_radius * sine[rnd_inner_angle_ix];
                     blast_to.x = (center_.x + couter_radius_dist_from_center * cosine[outer_angle_ix]) + rnd_outer_radius * cosine[rnd_inner_angle_ix];
@@ -130,14 +130,14 @@ namespace g80 {
         inline auto get_inner_radius_dist_from_center() -> Sint32 {return inner_radius_dist_from_center_;}
         inline auto get_outer_radius_dist_from_center() -> Sint32 {return outer_radius_dist_from_center_;}
 
-        inline auto get_blasts() const-> const Blasts<T> & {return blasts_;}
+        inline auto get_blasts() const-> const Blasts<F> & {return blasts_;}
 
     private:
         Sint32 sz_blast_steps_{10}, sz_blast_trail_{2};
         Point<Sint32> center_;
         Sint32 inner_radius_, outer_radius_, inner_radius_dist_from_center_, outer_radius_dist_from_center_;
-        Blasts<T> blasts_;
-        T f_;
+        Blasts<F> blasts_;
+        F outer_inner_ratio_;
     };
 
 }
