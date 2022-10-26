a268171e670a656a35da52780e441f20e04b25bf
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-10-26 11:35:02 +0800

    misc edits and added scenario

---
+++ include/test/common.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace g80::test {
+    const wchar_t *PASSED_TEXT{L"\033[32mPassed\033[0m"};
+    const wchar_t *FAILED_TEXT{L"\033[31mFailed\033[0m"};
+    const wchar_t *WARNING_TEXT{L"\033[33mWarning\033[0m"};     
+}
---
+++ include/test/scenario.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+namespace g80::test {
+
+    class scenario {
+    private:
+        const wchar_t   *name_;
+    };
+}
