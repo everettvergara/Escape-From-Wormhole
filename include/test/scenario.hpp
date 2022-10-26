bb041f49ba1fa27e51bf822ca1b0dac2c7c8a35d
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-10-26 12:12:33 +0800

    added scenario

--- include/test/scenario.hpp
+++ include/test/scenario.hpp
@@ -1,10 +1,25 @@
 #pragma once
 
+#include <functional>
 #include <vector>
+
 namespace g80::test {
 
+    struct script {
+        const wchar_t                   *name;
+        std::function<auto () -> bool>  fn;
+    };
+
     class scenario {
+    
     private:
-        const wchar_t   *name_;
+        const wchar_t       *name_;
+        std::vector<script> scripts_;
+
+    public:
+        scenario(const wchar_t *name) : name_(name) {}
+        virtual ~scenario() = default;
+        inline auto get_name() -> const wchar_t * {return name_;}
+        inline auto get_scripts() -> const std::vector<script> & {return scripts_;}
     };
 }
