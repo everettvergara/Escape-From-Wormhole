cc06c466a17f28a4270c4d2f28048c3ec10119ff
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-10-27 20:03:54 +0800

    added initialization

--- include/video/ts_base_point.hpp
+++ include/video/ts_base_point.hpp
@@ -16,22 +16,26 @@ namespace g80::worm::tdd {
     class ts_base_point : public scenario {
     private:
 
+        static constexpr T N{100};
+        static constexpr T O{std::is_signed_v<T> ? -N/4 : 0};
         std::vector<std::tuple<T, T>> xys_;
 
-        auto init(const size_t t) -> void {
-            
+        auto init(const size_t t) -> bool {
             xys_.reserve(t);
             for(size_t i{0}; i < t; ++i) {
-                T x{100 % }, y;
+                T x{N % (N/2) - O}, y{N % (N/2) - O};
                 xys_.emplace_back(std::tuple<T, T>(x, y));
+                std::cout << "x: " << x << ", " << y << "\n";
             }
+            return true;
         }
 
     public:
 
         ts_base_point(const wchar_t *name) : 
             scenario(name) {
-            // add_script(script(L"Always True", std::bind(&ts_scenario::always_true, this)));
+            add_script(script(L"Initialization", std::bind(&ts_base_point::init, this)));
         }
     };
 }
+
