75a8dc722079f685c85b71e254a66ba1b7293722
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-10-29 19:27:40 +0800

    misc edits

--- include/video/video.hpp
+++ include/video/video.hpp
@@ -1,5 +1,20 @@
 #pragma once
 
+#include "sys/common.hpp"
+#include "base_point.hpp"
+
 namespace g80::video {
+
+    using point = base_point<g80::sys::int_type>;
+    
+    
+
+    class video {
+    
+    private:
+
+    public:
+
+    };
 
 }
