28363bf421db70a48d90edd5162d78e21bf87d08
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-10-31 09:06:49 +0800

    completed window class

--- include/video/video.hpp
+++ include/video/video.hpp
@@ -15,8 +15,15 @@ namespace g80::video {
         SDL_Window *window_;
     public:
         window(const char *title, int x, int y, int w, int h, Uint32 flags) : window_(SDL_CreateWindow(title, x, y, w, h, flags)) {}
+        window(const window &) = delete;
+        window(window &&) = delete;
+        auto operator=(const window &) -> window & = delete;
+        auto operator=(window &&) -> window & = delete;
         ~window() {SDL_DestroyWindow(window_);}
+
+        inline auto is_valid() -> bool {return window_ != NULL;}
         inline auto get_handle() -> SDL_Window * {return window_;}
+        inline auto get_surface() -> SDL_Surface * {return SDL_GetWindowSurface(window_);}
     };
 
     class surface32 {
@@ -24,8 +31,8 @@ namespace g80::video {
         SDL_Surface *surface_;
     public:
         surface32(int w, int h, Uint32 format) : surface_(SDL_CreateRGBSurfaceWithFormat(0, w, h, 32, format)) {}
-        auto get_surface() -> SDL_Surface * {return surface_;}
         ~surface32() {SDL_FreeSurface(surface_);}
+        auto get_surface() -> SDL_Surface * {return surface_;}
     };
 
     class video {
