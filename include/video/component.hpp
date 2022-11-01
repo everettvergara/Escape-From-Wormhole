9b885eae7a1da0e3c854c267baf0627ecc47279c
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-01 20:41:40 +0800

    misc edits

--- include/video/component.hpp
+++ include/video/component.hpp
@@ -1,6 +1,8 @@
 #pragma once 
 
+
 #include <memory>
+#include <vector>
 
 namespace g80::game {
 
@@ -10,7 +12,8 @@ namespace g80::game {
         return next_component_type_id++;
     }
     
-    template<typename T> auto get_component_type_id() -> size_t {
+    template<typename T> 
+    auto get_component_type_id() -> size_t {
         static size_t component_type_id{get_next_component_type_id()};
         return component_type_id;
     }
@@ -26,6 +29,23 @@ namespace g80::game {
         virtual void update() = 0;
         virtual void render() = 0;
         virtual ~component() = 0;
+    };
+
+    class entity {
+    private:
+        std::vector<std::unique_ptr<component>> components_;
+        std::vector<size_t> component_types_;
+    public:
+        entity() {
+            // reserve components_.
+            // add_component_type
+        }
+
+        template<typename T, typename ...A> 
+        auto add_component_type(A &&...a) -> T& {
+            auto &p = components_.emplace_back(make_unique<T>(this, std::forward<A>(a)...));
+            component_types_.emplace_back();
+        }
     };
 
 }
