f5db4564667bf78eb0dde5ef89efbb146d105d6f
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-01 20:26:58 +0800

    added component

--- include/video/component.hpp
+++ include/video/component.hpp
@@ -10,15 +10,22 @@ namespace g80::game {
         return next_component_type_id++;
     }
     
-    template<typename T> auto get_component_id() -> size_t {
+    template<typename T> auto get_component_type_id() -> size_t {
         static size_t component_type_id{get_next_component_type_id()};
         return component_type_id;
     }
 
+    class entity;
+
     class component {
-
-
-
+    private:
+        entity *entity_;
+    public:
+        component(entity *e) : entity_(e) {}
+        virtual void init() = 0;
+        virtual void update() = 0;
+        virtual void render() = 0;
+        virtual ~component() = 0;
     };
 
 }
