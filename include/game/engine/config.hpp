0a8711c0d91c501a7d21d43ace75d06df086ccec
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-02 20:51:52 +0800

    added back_rgba

--- include/game/engine/config.hpp
+++ include/game/engine/config.hpp
@@ -9,15 +9,16 @@ namespace g80::game::engine {
     struct config {
         std::string title;
         int x, y, w, h;
+        Uint32 back_rgba;
         Uint32 flags;
         Uint16 FPS;
         Uint16 MSPF;
 
-        // Remove constructors. 
-        config(std::string title, int x = SDL_WINDOWPOS_CENTERED, int y = SDL_WINDOWPOS_CENTERED, int w = 1280, int h = 720, Uint32 flags = SDL_WINDOW_SHOWN, Uint16 FPS = 60) :
+        config(std::string title, int x = SDL_WINDOWPOS_CENTERED, int y = SDL_WINDOWPOS_CENTERED, int w = 1280, int h = 720, Uint32 back_rgba = 0, Uint32 flags = SDL_WINDOW_SHOWN, Uint16 FPS = 60) :
             title{std::move(title)}, 
             x{x}, y{y}, w{w}, h{h},
-            flags{flags}, FPS{FPS}, MSPF{static_cast<Uint16>(1000) / static_cast<Uint16>(FPS)} {
+            flags{flags}, FPS{FPS}, 
+            MSPF{static_cast<Uint16>(static_cast<Uint16>(1000) / FPS)} {
         }
 
         // config(config &&r) :
