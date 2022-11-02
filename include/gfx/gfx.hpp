3b4b07299b41124d011698e836d13bf3677e8b7e
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-02 19:30:47 +0800

    misc edits

---
+++ include/gfx/gfx.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SDL.h>
+
+namespace g80::gfx {
+
+    
+
+}
--- test.cpp
+++ test.cpp
@@ -10,17 +10,18 @@
 
 auto main(int argc, char *argv[]) -> int {
 
-    // using namespace g80::video;
-    // video::init();
-    // video worm;
-    // video::quit();
+    using namespace g80::video;
+    video::init();
+    video worm;
+    video::quit();
 
-    using namespace g80::ecs;
-    std::vector<std::unique_ptr<entity>> game_objs;
 
-    game_objs.reserve(100);
-    auto &p = game_objs.emplace_back(std::make_unique<player>());
+    // ECS Test
+    // ---
+    // using namespace g80::ecs;
+    // std::vector<std::unique_ptr<entity>> game_objs;
+    // game_objs.reserve(100);
+    // auto &p = game_objs.emplace_back(std::make_unique<player>());
 
-    // std::unique_ptr<entity> game_obj {std::make_unique<player>()};
     return 0;
 }
