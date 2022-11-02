f2de52de719b1aadc3fa4c8949b28785ca29f670
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-03 07:50:52 +0800

    added get_screen_plane

--- include/game/gfx/line.hpp
+++ include/game/gfx/line.hpp
@@ -5,11 +5,25 @@
 
 namespace g80::game::gfx {
 
-    using namespace g80::sys;
+    enum SCREEN_PLANE{TOP_LEFT = 0, TOP = 1, TOP_RIGHT = 2, LEFT = 3, ON_SCREEN = 4, RIGHT = 5, BOTTOM_LEFT = 6, BOTTOM = 7, BOTTOM_RIGHT = 8};
 
-    inline auto pset(SDL_Surface *s, const int_type x, const int_type y, const uint_type rgba) -> void {
-        RETURN_IF_NOT_WITHIN_BOUNDS(x, y, s->w, s->h);
-        *((static_cast<uint_type *>(s->pixels) + x) + (y * s->w)) = rgba;
+    auto get_screen_plane(SDL_Surface *s, const int_type x, const int_type y) -> SCREEN_PLANE {
+        if(x >= 0) [[likely]] {
+            if(x < s->w) [[likely]] {
+                if(y >= 0) [[likely]] {
+                    if(y < s->h) [[likely]] return ON_SCREEN;
+                    else return BOTTOM;
+                } else return TOP;
+            } else {
+                if(y < 0) return TOP_RIGHT;
+                else if(y >= s->h) return BOTTOM_RIGHT;
+                else return RIGHT;
+            }
+        } else {
+            if(y < 0) return TOP_LEFT;
+            else if(y >= s->h) return BOTTOM_LEFT;
+            else return LEFT;
+        }
     }
 
 
