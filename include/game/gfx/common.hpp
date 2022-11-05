a74d5d65556ec5056f80a065bedcada626c5cfb5
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-05 09:22:51 +0800

    removed get_screen_plane function bec. it's unncessary

--- include/game/gfx/common.hpp
+++ include/game/gfx/common.hpp
@@ -10,14 +10,14 @@ namespace g80::game::gfx {
     using g80::sys::fp_type;
     using point = base_point<int_type>;
 
-    inline auto within_bounds(const int_type x, const int_type y, const int_type w, const int_type h) -> bool {
+    inline auto is_point_within_bounds(const int_type x, const int_type y, const int_type w, const int_type h) -> bool {
         if(x < 0 || y < 0 || x >= w || y >= h) return false;
         return true;
     }
 
     #define GFX_SAFE_MODE 
     #ifdef GFX_SAFE_MODE
-        #define RETURN_IF_NOT_WITHIN_BOUNDS(x, y, w, h) if(!within_bounds(x, y, w, h)) return
+        #define RETURN_IF_NOT_WITHIN_BOUNDS(x, y, w, h) if(!is_point_within_bounds(x, y, w, h)) return
     #else
         #define RETURN_IF_NOT_WITHIN_BOUNDS(x, y, w, h)
     #endif
--- include/game/gfx/line.hpp
+++ include/game/gfx/line.hpp
@@ -8,24 +8,24 @@ namespace g80::game::gfx {
 
     enum SCREEN_PLANE{TOP_LEFT = 0, TOP = 1, TOP_RIGHT = 2, LEFT = 3, ON_SCREEN = 4, RIGHT = 5, BOTTOM_LEFT = 6, BOTTOM = 7, BOTTOM_RIGHT = 8};
 
-    auto get_screen_plane(SDL_Surface *s, const int_type x, const int_type y) -> SCREEN_PLANE {
-        if(x >= 0) [[likely]] {
-            if(x < s->w) [[likely]] {
-                if(y >= 0) [[likely]] {
-                    if(y < s->h) [[likely]] return ON_SCREEN;
-                    else return BOTTOM;
-                } else return TOP;
-            } else {
-                if(y < 0) return TOP_RIGHT;
-                else if(y >= s->h) return BOTTOM_RIGHT;
-                else return RIGHT;
-            }
-        } else {
-            if(y < 0) return TOP_LEFT;
-            else if(y >= s->h) return BOTTOM_LEFT;
-            else return LEFT;
-        }
-    }
+    // auto get_screen_plane(SDL_Surface *s, const int_type x, const int_type y) -> SCREEN_PLANE {
+    //     if(x >= 0) [[likely]] {
+    //         if(x < s->w) [[likely]] {
+    //             if(y >= 0) [[likely]] {
+    //                 if(y < s->h) [[likely]] return ON_SCREEN;
+    //                 else return BOTTOM;
+    //             } else return TOP;
+    //         } else {
+    //             if(y < 0) return TOP_RIGHT;
+    //             else if(y >= s->h) return BOTTOM_RIGHT;
+    //             else return RIGHT;
+    //         }
+    //     } else {
+    //         if(y < 0) return TOP_LEFT;
+    //         else if(y >= s->h) return BOTTOM_LEFT;
+    //         else return LEFT;
+    //     }
+    // }
 
     auto get_point_on_xintercept(int_type x, int_type y, int_type w, fp_type m) -> std::tuple<int_type, int_type> {
         if(w == 0) return {x, y};
@@ -41,8 +41,10 @@ namespace g80::game::gfx {
 
     }
 
-    auto line(SDL_Surface *s, const int_type x1, const int_type y1, const int_type x2, const int_type y2, const Uint32 rgba) -> void {
-    
+    auto line(SDL_Surface *s, int_type x1, int_type y1, int_type x2, int_type y2, const Uint32 rgba) -> void {
+        bool is_p1_within_bounds = is_point_within_bounds(x1, y1, s->w, s->h);
+        bool is_p2_within_bounds = is_point_within_bounds(x1, y1, s->w, s->h);
+
     }
 
 }
