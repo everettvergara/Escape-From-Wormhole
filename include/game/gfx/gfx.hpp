1e55e4c354d9840264574fad2fd75d0fb2f5dfae
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-05 23:13:10 +0800

    misc edits

--- include/game/gfx/gfx.hpp
+++ include/game/gfx/gfx.hpp
@@ -10,10 +10,13 @@ namespace g80::game::gfx {
     class gfx {
     private:
         SDL_Surface *s_;
+        int_type wb_, hb_;
     public:
-        gfx(SDL_Surface *s) : s_(s) {}
-        inline auto set_surface(SDL_Surface *s) -> void {s_ = s;}
+        gfx(SDL_Surface *s) : s_(s), wb_(s_->w - 1), hb_(s_->h - 1) {}
+        inline auto set_surface(SDL_Surface *s) -> void {s_ = s; wb_ = s_->w - 1; hb_ = s_->h - 1;}
         inline auto get_surface() const -> SDL_Surface * {return s_;}
+        inline auto get_wb() const -> int_type {return wb_;}
+        inline auto get_hb() const -> int_type {return hb_;}
         inline auto is_point_within_bounds(const int_type x, const int_type y) const -> bool {
             if(x < 0 || y < 0 || x >= s_->w || y >= s_->h) return false; 
             return true;
--- include/game/gfx/pset.hpp
+++ include/game/gfx/pset.hpp
@@ -11,16 +11,13 @@ namespace g80::game::gfx {
     class pset : public gfx {
     public:
         pset(SDL_Surface *s) : gfx(s) {}
-        auto draw(SDL_Surface *s, const uint_type x, const uint_type y, const uint_type rgba) -> void {
+        auto draw(const uint_type x, const uint_type y, const uint_type rgba) -> void {
             *((static_cast<uint_type *>(get_surface()->pixels) + x) + (y * get_surface()->w)) = rgba;
         }
-        auto draw_s(SDL_Surface *s, const uint_type x, const uint_type y, const uint_type rgba) -> void {
+        auto draw_s(const uint_type x, const uint_type y, const uint_type rgba) -> void {
             if(!is_point_within_bounds(x, y)) [[unlikely]] return;
             *((static_cast<uint_type *>(get_surface()->pixels) + x) + (y * get_surface()->w)) = rgba;
         }
-
-
-
     }
 
 
