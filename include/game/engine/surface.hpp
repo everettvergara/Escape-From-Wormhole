8faf620cfad6a9dc3eb8e08a72238857bb8861cb
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-07 14:23:04 +0800

    added surface from window

--- include/game/engine/surface.hpp
+++ include/game/engine/surface.hpp
@@ -7,13 +7,18 @@ namespace g80::game::engine {
     class surface {
     private:
         SDL_Surface *surface_;
+        bool is_surface_from_window_;
+        
     public:
-        surface(int w, int h, Uint32 format) : surface_(SDL_CreateRGBSurfaceWithFormat(0, w, h, 32, format)) {}
+        surface(SDL_Window *window) : surface_(SDL_GetWindowSurface(window)), is_surface_from_window_(true) {}
+        surface(int w, int h, Uint32 format) : surface_(SDL_CreateRGBSurfaceWithFormat(0, w, h, 32, format)), is_surface_from_window_(false) {}
+        
         surface(const surface &) = delete;
         surface(surface &&) = delete;
         auto operator=(const surface &) -> surface & = delete;
         auto operator=(surface &&) -> surface & = delete;
         ~surface() {SDL_FreeSurface(surface_);}
+
         auto is_valid() -> bool {return surface_ != NULL;}
         auto get_handle() -> SDL_Surface * {return surface_;}
     };
