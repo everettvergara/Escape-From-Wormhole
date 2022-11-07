71bed4276025d8247c02422631ab52a8b3491e97
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-07 19:21:24 +0800

    misc reorg

--- include/game/engine/surface.hpp
+++ include/game/engine/surface.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL.h>
+#include "game/gfx/pset.hpp"
 
 namespace g80::game::engine {
 
@@ -10,16 +11,30 @@ namespace g80::game::engine {
         bool is_surface_from_window_;
         
     public:
-        surface(SDL_Window *window) : surface_(SDL_GetWindowSurface(window)), is_surface_from_window_(true) {}
-        surface(int w, int h, Uint32 format) : surface_(SDL_CreateRGBSurfaceWithFormat(0, w, h, 32, format)), is_surface_from_window_(false) {}
+
+        // Window Surface
+        surface(SDL_Window *window) : 
+            surface_(SDL_GetWindowSurface(window)), is_surface_from_window_(true) {
+        }
         
+        // Custom Surface
+        surface(int w, int h, Uint32 format) : 
+            surface_(SDL_CreateRGBSurfaceWithFormat(0, w, h, 32, format)), is_surface_from_window_(false) {
+        }
+
+        // Deleted constructors and assignments 
         surface(const surface &) = delete;
         surface(surface &&) = delete;
         auto operator=(const surface &) -> surface & = delete;
         auto operator=(surface &&) -> surface & = delete;
-        ~surface() {SDL_FreeSurface(surface_);}
 
-        auto is_valid() -> bool {return surface_ != NULL;}
+        // Destructor
+        ~surface() {
+            if(!is_surface_from_window_)SDL_FreeSurface(surface_);
+        }
+
+        // Getters
         auto get_handle() -> SDL_Surface * {return surface_;}
+        auto is_valid() -> bool {return surface_ != NULL;}
     };
 }
