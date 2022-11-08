851b306fd1659f37c2aa2a1dc813c9dc92aab6a0
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-08 21:39:51 +0800

    better demo

--- include/game/worm/demo.hpp
+++ include/game/worm/demo.hpp
@@ -26,18 +26,18 @@ namespace g80::game::worm {
             g80::game::gfx::line line(&s);
             g80::game::gfx::palette pal(&s);
 
-            auto one_shade = pal.get_palette_gradient({{0, 0}, {99, 255}});
+            auto one_shade = pal.get_palette_gradient({{0, 0}, {100, 255}, {199, SDL_MapRGBA(s.get_handle()->format, 255, 0, 255, 255)}});
 
             SDL_FillRect(s.get_handle(), NULL, 0); 
-            // for(size_t i{0}; i< 1000; ++i) {
-            //     pixel.draw(rand() % s.get_handle()->w,
-            //             rand() % s.get_handle()->h,
-            //             SDL_MapRGBA(s.get_handle()->format, 255, 0, 0, 255));
-            // }
+            for(size_t i{0}; i< 1000; ++i) {
+                pixel.draw(rand() % s.get_handle()->w,
+                        rand() % s.get_handle()->h,
+                        SDL_MapRGBA(s.get_handle()->format, 255, 0, 0, 255));
+            }
             rads_ += 0.005;
             line.draw( point{s.get_cw(), s.get_ch()}, 
                         point{s.get_cw() + static_cast<int_type>(200 * SDL_cosf(rads_)), s.get_ch() + static_cast<int_type>(200 * SDL_sinf(rads_))}, 
-                    one_shade.value(), 0, 99);
+                    one_shade.value(), 0, 199);
             
             
             // line.draw( point{s.get_cw(), s.get_ch()}, 
