7927e852458a0b1833b4d9291e186ad7633ad701
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-08 18:30:01 +0800

    create a separate file for other starter program

---
+++ test-tdd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "game/engine/config.hpp"
+#include "game/worm/demo.hpp"
+
+
+// #include <vector>
+// #include <memory>
+// #include "ecs/sample.hpp"
+
+auto main(int argc, char *argv[]) -> int {
+
+    using namespace g80::game::worm;
+    video::init();
+    config c{"Hello World"};
+    demo worm;
+    worm.create_window(c);
+    worm.run(c.MSPF);
+    video::quit();
+
+
+    // ECS Test
+    // ---+
+    // using namespace g80::ecs;
+    // std::vector<std::unique_ptr<entity>> game_objs;
+    // game_objs.reserve(100);
+    // auto &p = game_objs.emplace_back(std::make_unique<player>());
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,12 +1,5 @@
-#include <iostream>
-
 #include "game/engine/config.hpp"
 #include "game/worm/demo.hpp"
-
-
-// #include <vector>
-// #include <memory>
-// #include "ecs/sample.hpp"
 
 auto main(int argc, char *argv[]) -> int {
 
@@ -17,14 +10,6 @@ auto main(int argc, char *argv[]) -> int {
     worm.create_window(c);
     worm.run(c.MSPF);
     video::quit();
-
-
-    // ECS Test
-    // ---+
-    // using namespace g80::ecs;
-    // std::vector<std::unique_ptr<entity>> game_objs;
-    // game_objs.reserve(100);
-    // auto &p = game_objs.emplace_back(std::make_unique<player>());
 
     return 0;
 }
