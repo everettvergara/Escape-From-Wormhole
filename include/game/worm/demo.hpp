44d00e3086b525ad1bfff23021ec21ae35d69c35
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-10 21:36:11 +0800

    finalized bugs

--- include/game/worm/demo.hpp
+++ include/game/worm/demo.hpp
@@ -33,7 +33,7 @@ namespace g80::game::worm {
 
             SDL_FillRect(s.get_handle(), NULL, 0); 
 
-            rect.draw(300, 300, 100, 200, SDL_MapRGBA(s.get_handle()->format, 255, 0, 255, 255), 0xff00ff00);
+            rect.draw(300, 270, 100, 200, SDL_MapRGBA(s.get_handle()->format, 255, 0, 255, 255), 0xff00ff00);
             
             rect.draw_s(500, 500, 100, 100, SDL_MapRGBA(s.get_handle()->format, 255, 0, 0, 255));
             rect.draw_s(-50, -50, 100, 100, SDL_MapRGBA(s.get_handle()->format, 255, 0, 0, 255));
