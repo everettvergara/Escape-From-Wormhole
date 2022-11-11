5d3139efa4cd33e86c5959ba107aac462aa66352
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-11 08:06:06 +0800

    misc edits

--- include/game/gfx/rect.hpp
+++ include/game/gfx/rect.hpp
@@ -76,20 +76,26 @@ namespace g80::game::gfx {
         }    
 
         inline auto draw_s(const int_type x, const int_type y, const int_type w, const int_type h, const Uint32 rgba) -> void {
-
             int_type sx, sy, mw, mh;
 
             // Starting X
-            if (0 > x && 0 <= x + w - 1) {  // Remember that we're comparing x2, so we need to deduct -1
-                sx = 0;
-                mw = w + x;
+            if (w >= 0) {
+                if (0 > x && 0 <= x + w - 1) {  // Remember that we're comparing x2, so we need to deduct -1
+                    sx = 0;
+                    mw = w + x;
 
-            } else if (x + w - 1 < 0 || x >= s_->get_handle()->w) {
-                return;
+                } else if (x + w - 1 < 0 || x >= s_->get_handle()->w) {
+                    return;
+
+                } else {
+                    sx = x;
+                    mw = w;
+                }
 
             } else {
-                sx = x;
-                mw = w;
+                if (x >= s_->get_handle()->w && x + w + 1 < s_->get_handle()->w) { 
+                    sx = s_->get_wb();
+                }
             }
 
             // Starting Y
@@ -115,19 +121,19 @@ namespace g80::game::gfx {
             auto *upper_left = (static_cast<Uint32 *>(s_->get_handle()->pixels) + sx) + (sy * s_->get_handle()->w);
             
             // Draw Top
-            if(y == sy) {
+            if(y == sy) [[likely]] {
                 auto *pixel_top = upper_left;
                 for (int i{0}; i < mw; ++i) *pixel_top++ = rgba;
             }
             
             // Draw Bottom
-            if (y + h - 1 < s_->get_handle()->h) {
+            if (y + h - 1 < s_->get_handle()->h) [[likely]] {
                 auto *pixel_bottom = upper_left + ((mh - 1) * s_->get_handle()->w);
                 for (int i{0}; i < mw; ++i) *pixel_bottom++ = rgba;
             }
             
             // Draw Left
-            if (x == sx) {
+            if (x == sx) [[likely]] {
                 auto *pixel_left = upper_left + (y == sy ? s_->get_handle()->w : 0);
                 auto d = (y + h - 1 < s_->get_handle()->h ? 2 : 1) + (y == sy ? 0 : -1);
                 for (int i{0}; i < mh - d; ++i) {
@@ -137,7 +143,7 @@ namespace g80::game::gfx {
             }
 
             // Draw Right
-            if (x + w - 1 < s_->get_handle()->w) {
+            if (x + w - 1 < s_->get_handle()->w) [[likely]] {
                 auto *pixel_right = upper_left + (y == sy ? s_->get_handle()->w : 0) + mw - 1;
                 auto d = (y + h - 1 < s_->get_handle()->h ? 2 : 1) + (y == sy ? 0 : -1);
                 for (int i{0}; i < mh - d; ++i) {
@@ -146,5 +152,77 @@ namespace g80::game::gfx {
                 }                
             }            
         }    
+
+        // inline auto draw_s(const int_type x, const int_type y, const int_type w, const int_type h, const Uint32 rgba) -> void {
+
+        //     int_type sx, sy, mw, mh;
+
+        //     // Starting X
+        //     if (0 > x && 0 <= x + w - 1) {  // Remember that we're comparing x2, so we need to deduct -1
+        //         sx = 0;
+        //         mw = w + x;
+
+        //     } else if (x + w - 1 < 0 || x >= s_->get_handle()->w) {
+        //         return;
+
+        //     } else {
+        //         sx = x;
+        //         mw = w;
+        //     }
+
+        //     // Starting Y
+        //     if (0 > y && 0 <= y + h - 1) {
+        //         sy = 0;
+        //         mh = h + y;
+
+        //     } else if (y + h - 1 < 0 || y >= s_->get_handle()->h) {
+        //         return;
+                
+        //     } else {
+        //         sy = y;
+        //         mh = h;
+        //     }
+
+        //     // Modified Width
+        //     if (sx + mw >= s_->get_handle()->w) mw -= sx + mw - s_->get_handle()->w;
+
+        //     // Modified Height
+        //     if (sy + mh >= s_->get_handle()->h) mh -= sy + mh - s_->get_handle()->h;
+
+
+        //     auto *upper_left = (static_cast<Uint32 *>(s_->get_handle()->pixels) + sx) + (sy * s_->get_handle()->w);
+            
+        //     // Draw Top
+        //     if(y == sy) {
+        //         auto *pixel_top = upper_left;
+        //         for (int i{0}; i < mw; ++i) *pixel_top++ = rgba;
+        //     }
+            
+        //     // Draw Bottom
+        //     if (y + h - 1 < s_->get_handle()->h) {
+        //         auto *pixel_bottom = upper_left + ((mh - 1) * s_->get_handle()->w);
+        //         for (int i{0}; i < mw; ++i) *pixel_bottom++ = rgba;
+        //     }
+            
+        //     // Draw Left
+        //     if (x == sx) {
+        //         auto *pixel_left = upper_left + (y == sy ? s_->get_handle()->w : 0);
+        //         auto d = (y + h - 1 < s_->get_handle()->h ? 2 : 1) + (y == sy ? 0 : -1);
+        //         for (int i{0}; i < mh - d; ++i) {
+        //             *pixel_left = rgba;
+        //             pixel_left += s_->get_handle()->w;
+        //         }
+        //     }
+
+        //     // Draw Right
+        //     if (x + w - 1 < s_->get_handle()->w) {
+        //         auto *pixel_right = upper_left + (y == sy ? s_->get_handle()->w : 0) + mw - 1;
+        //         auto d = (y + h - 1 < s_->get_handle()->h ? 2 : 1) + (y == sy ? 0 : -1);
+        //         for (int i{0}; i < mh - d; ++i) {
+        //             *pixel_right = rgba;
+        //             pixel_right += s_->get_handle()->w;
+        //         }                
+        //     }            
+        // }            
     };
 }
