aca14bca92eb54dff6427dfd7d9c4e0ab210ea63
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-20 07:09:59 +0800

    reorg pixel gfx

--- include/game/gfx/pixel.hpp
+++ include/game/gfx/pixel.hpp
@@ -5,25 +5,37 @@
 namespace g80::game::gfx {
 
     using namespace g80::game::engine;
-
+    
     class pixel {
-
     private:
-
         surface *s_;
 
     public:
-
-        pixel(surface *s) : s_(s) {
-        }
-
-        inline auto draw(const point &p, const Uint32 rgba) -> void {
-            *((static_cast<Uint32 *>(s_->get_handle()->pixels) + p.x) + (p.y * s_->get_handle()->w)) = rgba;
-        }
-
-        auto draw_s(const point &p, const Uint32 rgba) -> void {
-            if(!s_->is_point_within_bounds(p)) return;
-            draw(p, rgba);
-        }
+        pixel(surface *s);
+        auto draw(const int_type x, const int_type y, const Uint32 rgba) -> void;
+        auto draw(const point &p, const Uint32 rgba) -> void;
+        auto draw_s(const int_type x, const int_type y, const Uint32 rgba) -> void;
+        auto draw_s(const point &p, const Uint32 rgba) -> void;
     };
+
+    pixel::pixel(surface *s) : s_(s) {
+    }
+
+    auto pixel::draw(const int_type x, const int_type y, const Uint32 rgba) -> void {
+        *((static_cast<Uint32 *>(s_->get_handle()->pixels) + x) + (y * s_->get_handle()->w)) = rgba;
+    }
+
+    auto pixel::draw(const point &p, const Uint32 rgba) -> void {
+        *((static_cast<Uint32 *>(s_->get_handle()->pixels) + p.x) + (p.y * s_->get_handle()->w)) = rgba;
+    }
+
+    auto pixel::draw_s(const int_type x, const int_type y, const Uint32 rgba) -> void {
+        if(!s_->is_point_within_bounds(x, y)) return;
+        draw(x, y, rgba);
+    }
+
+    auto pixel::draw_s(const point &p, const Uint32 rgba) -> void {
+        if(!s_->is_point_within_bounds(p)) return;
+        draw(p, rgba);
+    }
 }
