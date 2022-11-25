c5566a7f05fd7d98336a0ba7a3fd2a9118e76188
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-25 19:08:10 +0800

    misc edits

--- include/game/gfx/circle.hpp
+++ include/game/gfx/circle.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stack>
 #include "game/engine/surface.hpp" 
 
 namespace g80::game::gfx {
@@ -17,6 +18,7 @@ namespace g80::game::gfx {
     };
 
     circle::circle(surface *s) : s_(s) {
+
     }
 
     auto circle::draw(const point &p, const int_type r, Uint32 rgba) -> void {
@@ -24,7 +26,7 @@ namespace g80::game::gfx {
         auto center = static_cast<Uint32 *>(s_->get_handle()->pixels);
 
         int_type x = r, y = 0;
-        int_type bx = x * surface_->w, by = 0;
+        int_type bx = x * s_->w, by = 0;
 
         Sint32 dx = 1 - (r << 1);
         Sint32 dy = 1;
@@ -45,11 +47,11 @@ namespace g80::game::gfx {
             dy += 2;
             if ((re << 1) + dx > 0) {
                 --x;
-                bx -= surface_->w;
+                bx -= s_->w;
                 re += dx;
                 dx += 2;
             }
-            by += surface_->w;
+            by += s_->w;
         }
     }
 }
