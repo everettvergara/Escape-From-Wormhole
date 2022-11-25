067e311f5c69d6cdad7940b788792109020bc467
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-26 02:15:20 +0800

    added symmetrical

--- include/game/gfx/circle.hpp
+++ include/game/gfx/circle.hpp
@@ -25,19 +25,23 @@ namespace g80::game::gfx {
         int_type slow = r, fast = 0;
         int_type bx = slow * s_->get_w(), by = 0;
 
-        Sint32 dx = 1 - (r << 1);
-        Sint32 dy = 1;
-        Sint32 re = 0;
+        auto center_r = center + r;
+        auto center_ded_r = center - r;
+        
+
+        int_type dx = 1 - (r << 1);
+        int_type dy = 1;
+        int_type re = 0;
 
         while (slow >= fast) {
-            *(center + slow - by) = rgba;  // Upper Right: Bottom, 
-            *(center + fast - bx) = rgba;  // Upper Right: Top, y++, x--
-            *(center - fast - bx) = rgba;  // Upper Left: Top 
-            *(center - slow - by) = rgba;  // Upper Left: Bottom,
+            *(center_r - by) = rgba;  // Upper Right: Bottom, 
+            *(center - bx) = rgba;  // Upper Right: Top, y++, x--
+            *(center - bx) = rgba;  // Upper Left: Top 
+            *(center_ded_r - by) = rgba;  // Upper Left: Bottom,
             *(center - slow + by) = rgba;  // Bottom Left: Top
-            *(center - fast + bx) = rgba;  // Bottom Left: Bottom 
-            *(center + fast + bx) = rgba;  // Bottom Right: Bottom 
-            *(center + slow + by) = rgba;  // Bottom Right: Top
+            *(center + bx) = rgba;  // Bottom Left: Bottom 
+            *(center + bx) = rgba;  // Bottom Right: Bottom 
+            *(center_r + by) = rgba;  // Bottom Right: Top
 
             ++fast;
             re += dy;
