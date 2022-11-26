6b507ce5d101b20b3929545e367ea19a357c3b88
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-26 21:40:04 +0800

    bug corrections on naming conventions

--- include/game/gfx/circle.hpp
+++ include/game/gfx/circle.hpp
@@ -23,28 +23,29 @@ namespace g80::game::gfx {
         auto center = static_cast<Uint32 *>(s_->get_handle()->pixels) + p.y * s_->get_w() + p.x;
 
 
-        int_type fast_adder_from_0 = 0;
-        int_type slow_adder_from_0 = r * s_->get_w();
+        int_type fast_adder_by_y_inc = 0;
+        int_type fast_adder_by_x_inc = 0;
 
-        int_type fast_adder_from_r = r * s_->get_w();
-        int_type slow_adder_from_r = 0;
-
+        int_type slow_adder_by_x_dec = r;
+        int_type slow_adder_by_y_dec = r * s_->get_w();
 
         int_type dx = 1 - (r << 1);
         int_type dy = 1;
         int_type re = 0;
 
-        while (slow_adder_from_0 >= fast_adder_from_0) {
-            *(center + fast_adder_from_r - slow_adder_from_r) = rgba;     // Q1: bottom
+        while (slow_adder_by_x_dec >= fast_adder_by_x_inc) {
+            *(center - fast_adder_by_y_inc + slow_adder_by_x_dec) = rgba;     // Q1: bottom
 
             re += dy;
             dy += 2;
             if ((re << 1) + dx > 0) {
                 re += dx;
                 dx += 2;
-                --slow_adder_from_0;
+                slow_adder_by_x_dec -= 1;
+                slow_adder_by_y_dec -= s_->get_w();
             }
-            fast_adder_from_0 += s_->get_w();
+            fast_adder_by_x_inc += 1;
+            fast_adder_by_y_inc += s_->get_w();
         }
     }
 } 
