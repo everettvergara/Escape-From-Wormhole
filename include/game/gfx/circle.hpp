0a6354e6b36340c04848ae1939438d6db39d441e
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-11-27 21:52:54 +0800

    added circle with mask

--- include/game/gfx/circle.hpp
+++ include/game/gfx/circle.hpp
@@ -13,21 +13,22 @@ namespace g80::game::gfx {
 
     public:
         circle(surface *s);
-        auto draw(const point &p, const int_type r, Uint32 rgba) -> void;
+        auto draw(const point &p, const int_type r, const Uint32 rgba) -> void;
+        auto draw(const point &p, const int_type r, const Uint32 rgba, const Uint32 mask) -> void;
     };
 
     circle::circle(surface *s) : s_(s) {
     }
 
-    auto circle::draw(const point &p, const int_type r, Uint32 rgba) -> void {
+    auto circle::draw(const point &p, const int_type r, const Uint32 rgba) -> void {
         auto center = static_cast<Uint32 *>(s_->get_handle()->pixels) + p.y * s_->get_w() + p.x;
         int_type fast_adder_by_y_inc = 0;
         int_type fast_adder_by_x_inc = 0;
         int_type slow_adder_by_x_dec = r;
         int_type slow_adder_by_y_dec = r * s_->get_w();
-        int_type dx = 1 - (r << 1);
-        int_type dy = 1;
-        int_type re = 0;
+        int_type delta_x = 1 - (r << 1);
+        int_type delta_y = 1;
+        int_type radius_error = 0;
         while(slow_adder_by_x_dec > fast_adder_by_x_inc) {
             *(center - fast_adder_by_y_inc + slow_adder_by_x_dec) = rgba;     // Q1: bottom
             *(center + fast_adder_by_x_inc - slow_adder_by_y_dec) = rgba;     // Q1: top
@@ -37,17 +38,58 @@ namespace g80::game::gfx {
             *(center - fast_adder_by_x_inc + slow_adder_by_y_dec) = rgba;     // Q3: bottom
             *(center + fast_adder_by_x_inc + slow_adder_by_y_dec) = rgba;     // Q4: bottom
             *(center + fast_adder_by_y_inc + slow_adder_by_x_dec) = rgba;     // Q4: top
-            re += dy;
-            dy += 2;
-            if((re << 1) + dx > 0) {
-                re += dx;
-                dx += 2;
+            radius_error += delta_y;
+            delta_y += 2;
+            if((radius_error << 1) + delta_x > 0) {
+                radius_error += delta_x;
+                delta_x += 2;
                 slow_adder_by_x_dec -= 1;
                 slow_adder_by_y_dec -= s_->get_w();
             }
             fast_adder_by_x_inc += 1;
             fast_adder_by_y_inc += s_->get_w();
         }
+        exit(0);
     }
+
+    auto circle::draw(const point &p, const int_type r, const Uint32 rgba, const Uint32 mask) -> void {
+        auto center = static_cast<Uint32 *>(s_->get_handle()->pixels) + p.y * s_->get_w() + p.x;
+        int_type fast_adder_by_y_inc = 0;
+        int_type fast_adder_by_x_inc = 0;
+        int_type slow_adder_by_x_dec = r;
+        int_type slow_adder_by_y_dec = r * s_->get_w();
+        int_type delta_x = 1 - (r << 1);
+        int_type delta_y = 1;
+        int_type radius_error = 0;
+        
+        int_type tctr = 31;
+        auto tmask = mask;
+        while(slow_adder_by_x_dec > fast_adder_by_x_inc) {
+            if(tmask & 1) {
+                *(center - fast_adder_by_y_inc + slow_adder_by_x_dec) = rgba;
+                *(center + fast_adder_by_x_inc - slow_adder_by_y_dec) = rgba;     
+                *(center - fast_adder_by_x_inc - slow_adder_by_y_dec) = rgba;
+                *(center - fast_adder_by_y_inc - slow_adder_by_x_dec) = rgba;
+                *(center + fast_adder_by_y_inc - slow_adder_by_x_dec) = rgba;
+                *(center - fast_adder_by_x_inc + slow_adder_by_y_dec) = rgba;
+                *(center + fast_adder_by_x_inc + slow_adder_by_y_dec) = rgba;
+                *(center + fast_adder_by_y_inc + slow_adder_by_x_dec) = rgba;
+            }
+            radius_error += delta_y;
+            delta_y += 2;
+            if((radius_error << 1) + delta_x > 0) {
+                radius_error += delta_x;
+                delta_x += 2;
+                slow_adder_by_x_dec -= 1;
+                slow_adder_by_y_dec -= s_->get_w();
+            }
+            fast_adder_by_x_inc += 1;
+            fast_adder_by_y_inc += s_->get_w();
+            tctr -= tctr > 0 ? 1 : -31;
+        }
+        exit(0);
+    }
+
+
 } 
 
